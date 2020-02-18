5212ff5f786de8ae5cd59a9c4b6c87eaa0635ef1
Devwrat <devwratjoshi1234@gmail.com>
2020-02-18 17:04:26 +0900

    Add config file

---
+++ config.py
@@ -0,0 +1,25 @@
+pins = {
+            "I2C_SCL_pin": 25,
+            "I2C_SDA_pin": 26,
+            "servo_pin": 32
+        }
+
+
+modes = {
+            "set_timer" : True, 
+            "check_for_update_time": 60000, 
+            
+        }
+bunri_wifi= {
+                "ssid": b'ssid',
+                "pass": 'password'
+            }
+
+# The follwing are the standard return values for functions from different modules
+# white and black zone returns are the values returned by the getZone function from the 
+# APDS9960 sensor control module
+
+returns={
+            "white_zone_return": 0,
+            "black_zone_return": 1
+        }
