5b84e3b37ee347152336a0a3fe815c5afe5fb346
Devwrat <devwratjoshi1234@gmail.com>
2020-02-18 17:03:54 +0900

    Modify main.py to stop using color sensor

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time
 import config
-import motorcontrol as mc
-import APDS9960 as cs
+#import motorcontrol as mc
+#import APDS9960 as cs
 
 maxTime = 120000 # Execute the script for this long in ms
 start_time = time.ticks_ms()
@@ -10,21 +10,21 @@ sensor = cs.APDS9960()
 servo = mc.Servo()
 time.sleep(2)
 
-while time.ticks_diff(time.ticks_ms(), start_time) < maxTime:
+#while time.ticks_diff(time.ticks_ms(), start_time) < maxTime:
 
-    if not sensor.getZone() == last_zone: #This means a size change is required
-        if(sensor.getZone() == config.returns["white_zone_return"]):
-            servo.changeSize('small')
-            print("time to become small")
-        elif(sensor.getZone() == config.returns["black_zone_return"]):
-            servo.changeSize('big')
-            print("time to become big")
-        time.sleep(2)
-        last_zone = sensor.getZone()
-
-
-    time.sleep(1)
-    print("Executing the function")
+#    if not sensor.getZone() == last_zone: #This means a size change is required
+ #       if(sensor.getZone() == config.returns["white_zone_return"]):
+  #          servo.changeSize('small')
+   #         print("time to become small")
+    #    elif(sensor.getZone() == config.returns["black_zone_return"]):
+     #       servo.changeSize('big')
+      #      print("time to become big")
+       # time.sleep(2)
+        #last_zone = sensor.getZone()
 
 
-print("Done with the program")
+    #time.sleep(1)
+    #print("Executing the function")
+
+
+#print("Done with the program")
